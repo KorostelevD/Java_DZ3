4542e6937e643f9a93261455d02a2d3237784f6f
Dmytro <admin@gmail.com>
2024-08-20 09:13:06 +0300

    unit1

--- src/DZ/NameGen.java
+++ src/DZ/NameGen.java
@@ -1,11 +1,36 @@
 package DZ;
+import java.util.Random; //Додаю Random
 
 public class NameGen {
     public static void main(String[] args) {
-        String[] firstName = {"Іван", "Петро", "Олександр", "Віктор", "Дмитро", "Андрій", "Михайло", "Юрій", "Олег", "Василь"};
-        String[] middleName = {"Іванович", "Петрович", "Олександрович", "Вікторович", "Дмитрович", "Андрійович", "Михайлович", "Юрійович", "Олегович", "Васильович"};
-        String[] lastName = {"Мельник", "Шевченко", "Коваль", "Бондаренко", "Ткаченко", "Романенко", "Лисенко", "Зайцев", "Гриценко", "Кравченко"};
+        // Створюю масиви імен, по батькові та прізвищ
+        String[] firstName = {"Максим", "Роман", "Денис", "Віктор", "Дмитро", "Андрій", "Михайло", "Юрій", "Олег", "Василь"};
+        String[] middleName = {"Юрійович", "Петрович", "Олександрович", "Вікторович", "Дмитрович", "Андрійович", "Михайлович", "Іванович", "Олегович", "Васильович"};
+        String[] lastName = {"Петренко", "Шевченко", "Коваль", "Бондаренко", "Ткаченко", "Романенко", "Лисенко", "Зайцев", "Гриценко", "Кравченко"};
 
+        // Створюємо об'єкт Random для генерації випадкових чисел
+        Random random = new Random();
+
+        // Створюємо StringBuilder для збереження результату
+        StringBuilder result = new StringBuilder();
+
+
+
+        // Цикл для генерації 10 випадкових поєднань
+        for (int i = 0; i < 10; i++) {
+            // Генеруємо випадкові індекси для масивів, використовуючи Метод int nextInt(int max). 0 входить до діапазону, max — не входить. firstNames.length — це довжина масиву
+            int firstNameIndex = random.nextInt(firstName.length);
+            int middleNameIndex = random.nextInt(middleName.length);
+            int lastNameIndex = random.nextInt(lastName.length);
+
+            // Формуємо рядок з номером, прізвищем, ім'ям та по батькові, додаємо дані за допомогою append
+            result.append(i + 1).append("\t") //Додаємо +1 щоб небуло 0 - го елемента
+                    .append(lastName[lastNameIndex]).append("\t")
+                    .append(firstName[firstNameIndex]).append("\t")
+                    .append(middleName[middleNameIndex]).append("\n");
+        }
+        // Виводимо результат у консоль
+        System.out.println(result.toString());
+
+     }
     }
-
-}
