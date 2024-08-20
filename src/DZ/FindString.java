5c231cba2228f4ddc6b71b20f9a9380f553ff9fa
Dmitro Korostelyov <admin@gmail.com>
2024-08-20 15:44:06 +0300

    Finish

--- src/DZ/FindString.java
+++ src/DZ/FindString.java
@@ -14,26 +14,26 @@ public class FindString {
 
         // 1. Знайти та отримати підрядки адрес сайтів
         System.out.println("Адреси сайтів у нижньому регістрі:");
-        extractAndPrintUrls(str);
+        extractAndPrintUrls(str); //виклик функції для String str
 
         // 2. Знайти усі входження підрядка "String"
-        System.out.println("\nПозиції підрядка 'String' з урахуванням регістру:");
+        System.out.println("\n Позиції підрядка 'String' з урахуванням регістру:");
         findSubstringPositions(str, "String", false);
 
-        System.out.println("\nПозиції підрядка 'String' незалежно від регістру:");
+        System.out.println("\n Позиції підрядка 'String' незалежно від регістру:");
         findSubstringPositions(str, "String", true);
     }
 
     // Метод для отримання підрядків адрес сайтів
-    private static void extractAndPrintUrls(String str) {
-        String lowerCaseStr = str.toLowerCase();
-        int start = 0;
-        while ((start = lowerCaseStr.indexOf("https://", start)) != -1) {
-            int end = lowerCaseStr.indexOf( "\n", start);
+    private static void extractAndPrintUrls(String str1) {
+        String lowerCaseStr = str1.toLowerCase(); //переводимо до нижнього регістру
+        int start = 0; // початкове значення рядка з якого кочинається пошук
+        while ((start = lowerCaseStr.indexOf("https://", start)) != -1) { //якщо нічого не знайденл, = -1 вихід из циклу
+            int end = lowerCaseStr.indexOf( "\n", start); //знаходимо кінець рядка
             if (end == -1) {
                 end = lowerCaseStr.length();
             }
-            System.out.println(lowerCaseStr.substring(start, end));
+            System.out.println(lowerCaseStr.substring(start, end)); // виводимо рядок с початку і до кінця
             start = end;
         }
     }
@@ -43,7 +43,7 @@ public class FindString {
         int length = target.length();
         for (int i = 0; i <= str.length() - length; i++) {
             if (str.regionMatches(ignoreCase, i, target, 0, length)) {
-                System.out.println("Знайдено підрядок на позиції: " + i);
+                System.out.println("Знайдено підрядок на позиції: " + (i+1));
             }
         }
     }
